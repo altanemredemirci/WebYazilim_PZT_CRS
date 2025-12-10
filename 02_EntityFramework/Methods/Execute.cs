d3f111d89522555086f3b2774c56b10f4111f4b1
altanemredemirci <altan_emre_1989@hotmail.com>
2025-12-10 21:36:13 +0300

    Database First

---
+++ 02_EntityFramework/Methods/Execute.cs
@@ -0,0 +1,128 @@
+﻿using _02_EntityFramework.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_EntityFramework.Methods
+{
+    internal class Execute
+    {
+
+        public static NorthwndContext db = new NorthwndContext();
+
+        #region Ürünler Listesi
+
+        public static List<Product> GetProducts()
+        {
+            //LINQtoSQL
+
+            //var products = from p in db.Products
+            //               select p;
+
+            //return products.ToList();
+
+
+            //LINQ
+            var products = db.Products.ToList();
+
+            return products;
+        }
+
+        #endregion
+
+        #region Belirli Bir Kategorideki Ürünler
+
+        public static List<Product> GetProductsByCategory(int categoryId)
+        {
+            //LINQtoSQL
+            //var products = from p in db.Products
+            //               where p.CategoryId == categoryId
+            //               select p;
+
+            //return products.ToList();
+
+            //LINQ
+            var products = db.Products.Where(p => p.CategoryId == categoryId).ToList();
+
+            return products;
+        }
+
+
+
+
+        #endregion
+
+        #region Fiyat 20'den Büyük Ürünler
+
+        public static List<Product> GetProductsByPrice()
+        {
+            var products = db.Products.Where(i => i.UnitPrice > 20).ToList();
+
+            return products;
+        }
+
+        #endregion
+
+        #region Ürün ve Kategori Bilgilerini Birleştir
+        public static void GetCategoriesByProducts()
+        {
+            // select ProductName, CategoryName, UnitPrice from Products p
+            // join Categories c on p.CategoryID = c.CategoryID
+
+
+            var liste = db.Products
+                .Join(db.Categories,
+                p => p.CategoryId,
+                c => c.CategoryId,
+                (p, c) => new
+                {
+                    ProductName = p.ProductName,
+                    CategoryName = c.CategoryName,
+                    UnitPrice = p.UnitPrice
+                })
+                .ToList();
+        }
+
+
+        #endregion
+
+        #region Sipariş Detayları ile Ürün Bilgisi
+        
+        public static void GetOrderDetails()
+        {
+            var orderDetails = db.OrderDetails
+                .Join(db.Products,
+                od => od.ProductId,
+                p => p.ProductId,
+                (od, p) => new
+                {
+                    OrderID = od.OrderId,
+                    ProductName = p.ProductName,
+                    Quantity = od.Quantity,
+                    UnitPrice = od.UnitPrice,
+                    Total = od.Quantity * od.UnitPrice
+                })
+                .ToList();
+        }
+        #endregion
+
+        #region Kategorilere Göre Ürün Sayısı
+
+        public static void GetCategoryProductCount()
+        {
+            var productCountByCateogry = db.Products
+                .GroupBy(p => p.Category.CategoryName)
+                .Select(g => new
+                {
+                    CategoryName = g.Key,
+                    ProductCount = g.Count()
+                })
+                .OrderByDescending(x => x.ProductCount)
+                .ToList();
+        }
+
+        #endregion
+    }
+}
---
+++ 02_EntityFramework/Program.cs
@@ -0,0 +1,64 @@
+﻿using _02_EntityFramework.Entities;
+using _02_EntityFramework.Methods;
+
+namespace _02_EntityFramework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //C# ile SQL bağlayan yapılara ORM(Object Relation Mapping)
+            /*
+             ENTITYFRAMEWORK
+            - Code First
+            - Database First
+            - Modal First
+            - Code First(Varolan Database)
+             
+
+            DATABASE FIRST:Hali hazırda varolan bir database'in C# bağlanması ilgili classların otomatik oluşmasını sağlar.
+
+
+
+            SQL Server Authentication
+            "Server=202-3\SQLDERS; Database=NORTHWND; Uid=sa; Pwd=1;"
+
+            Windows Authentication
+            "Server=202-3\SQLDERS; Database=NORTHWND; TrustServerCertificate=true; Trusted_Connection=true"
+            
+             
+             DATABASE FIRST
+            ****************
+            Scaffold-DbContext "Server=202-3\SQLDERS;Database=Northwind;Trusted_Connection=True; TrustServerCertificate=true;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Entities
+             
+
+            Kurulan Paketler
+            *Microsoft.EntityFramework 8.0.22
+            *Microsoft.EntityFrameworkCore.SqlServer
+            *Microsoft.EntityFrameworkCore.Tools
+             */
+
+            //NorthwndContext db = new NorthwndContext();
+
+            //List<Category> categories = db.Categories.ToList();
+
+
+            //Ürünler Listesi
+            //var products = Execute.GetProducts();
+
+            //Belirli Bir Kategorideki Ürünler
+            //var products = Execute.GetProductsByCategory(1);
+
+            //Ürün ve Kategori Bilgilerini Birleştir
+            //Execute.GetCategoriesByProducts();
+
+            //Sipariş Detayları ile Ürün Bilgisi
+            //Execute.GetOrderDetails();
+
+            //Kategorilere Göre Ürün Sayısı
+            Execute.GetCategoryProductCount();
+
+
+        }
+    }
+}
